7fe8bb0c3038812559b418c276975e481a54be07
paulmax <ipaulmax@icloud.com>
2022-08-22 16:16:08 +0300

    Additional Task & Fixes 1. Fixed:	warning in /* TASK 2.4.3 */ of class ArraysAdditionalTasks{} 2. Added:	file BubbleSort.pdf to image bubble sorting with block sceme

--- src/home_work_2/arrays/ArraysAdditionalTasks.java
+++ src/home_work_2/arrays/ArraysAdditionalTasks.java
@@ -64,7 +64,7 @@ public class ArraysAdditionalTasks {
                 System.out.print(element + " ");
             }
         }
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("——————————————————————————————————————————————————————");
     }
 
