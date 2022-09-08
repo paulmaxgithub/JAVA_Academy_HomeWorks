857649885aa116e85bd7c5e88a293559a158130a
paulmax <ipaulmax@icloud.com>
2022-09-08 17:01:30 +0300

    HM_3_TASK1 modified:	WithoutCalculatorMain.java (refactoring)

--- src/home_work_3/runners/WithoutCalculatorMain.java
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -15,52 +15,29 @@ public class WithoutCalculatorMain {
         // STEP 2. - result of STEP 1. ^ 2
         result = exponentiate(result, 2);
         // STEP 3. - 15 * 7
-        int x = multiply(15, 7);
+        double x = multiply(15, 7);
         // STEP 4. - (15 * 7) + (28 / 5) ^ 2
         result = add(result, x);
         // STEP 5. - result of STEP 4. + 4.1
         result = add(result, 4.1);
 
         //SHOW RESULT
-        System.out.println(result);
+        System.out.println(round(result, 2));
     }
 
-    private static int add(int a, int b) {
-        return a + b;
-    }
-    private static double add(double a, double b) {
-        return a + b;
-    }
+    private static double add(double a, double b) { return a + b; }
 
-/*
- private static void subtract(int a, int b) {}
- private static void subtract(double a, double b) {}
-*/
+    private static double multiply(double a, double b) { return a * b; }
 
-    private static int multiply(int a, int b) {
-        return a * b;
-    }
-    private static double multiply(double a, double b) {
-        return a * b;
-    }
+    private static double divide(double a, double b) { return a / b; }
 
-    private static int divide(int a, int b) {
-        return a / b;
-    }
-    private static double divide(double a, double b) {
-        return a / b;
-    }
+    private static double exponentiate(double number, int exp) { return (int)number ^ exp ; }
 
-    private static double exponentiate(int number, int exp) {
-        return Math.pow(number, exp);
-    }
-    private static double exponentiate(double number, double exp) {
-        return round(Math.pow(number, exp), 2);
-    }
-
-    /*
-    https://stackoverflow.com/questions/322749/retain-precision
-    -with-double-in-java#:~:text=Add%20a%20comment-,173,-As%20others%20have
+    /**
+     * <li><code>The link below describes the problem and suggests a solution</code>
+     * <br><br>
+     * https://stackoverflow.com/questions/322749/retain-precision
+     -with-double-in-java#:~:text=Add%20a%20comment-,173,-As%20others%20have
      */
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
